6a48bf564c9241452286dce5ee51d5f697ddeb2f
Pedro <pedrovirmond@gmail.com>
2024-10-27 17:12:21 -0300

    commit 27/10 17:12

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
-// Elementos de data e hora
+
 const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
-// Elementos de botão e alerta
 const btnBaterPonto = document.getElementById("btn-bater-ponto");
 const divAlertaRegistroPonto = document.getElementById("alerta-registro-ponto");
 const btnDialogFechar = document.getElementById("btn-dialog-fechar");
 const dialogPonto = document.getElementById("dialog-ponto");
 
-// Atualização da data e hora
 function atualizarDataHora() {
     diaSemana.textContent = new Date().toLocaleDateString('pt-BR', { weekday: 'long' });
     diaMesAno.textContent = new Date().toLocaleDateString('pt-BR');
@@ -17,7 +15,6 @@ function atualizarDataHora() {
 }
 setInterval(atualizarDataHora, 1000);
 
-// Função para salvar e recuperar registros do LocalStorage
 function salvarRegistro(ponto) {
     const registros = JSON.parse(localStorage.getItem("registros") || "[]");
     registros.push(ponto);
@@ -28,7 +25,6 @@ function obterRegistros() {
     return JSON.parse(localStorage.getItem("registros") || "[]");
 }
 
-// Mostrar ou ocultar alerta
 function mostrarAlerta() {
     divAlertaRegistroPonto.classList.add("show");
     setTimeout(() => {
@@ -36,7 +32,6 @@ function mostrarAlerta() {
     }, 5000);
 }
 
-// Registrar ponto
 btnBaterPonto.addEventListener("click", () => {
     dialogPonto.showModal();
 });
@@ -45,7 +40,6 @@ btnDialogFechar.addEventListener("click", () => {
     dialogPonto.close();
 });
 
-// Registrar ponto e salvar
 document.getElementById("btn-dialog-bater-ponto").addEventListener("click", () => {
     const tipo = document.getElementById("tipos-ponto").value;
     const data = new Date().toLocaleDateString('pt-BR');
